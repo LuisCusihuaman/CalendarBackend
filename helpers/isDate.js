a9815e2e8667decbbd8cd3520956f5e2aa0bbfcf
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 03:39:39 -0300

    chore: validated necesary fields in posts requests

---
+++ helpers/isDate.js
@@ -0,0 +1,13 @@
+const moment = require('moment');
+const isDate = (value) => {
+    if (!value) {
+        return false;
+    }
+    const fecha = moment(value);
+    if (fecha.isValid()) {
+        return true;
+    } else {
+        return false;
+    }
+};
+module.exports = { isDate };
--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,7 @@
 // /api/events
 const { Router } = require('express');
 const router = Router();
+const {} = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const {
     getEventos,
@@ -9,12 +10,24 @@ const {
     actualizarEvento,
     eliminarEvento,
 } = require('../controllers/events');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
 
 router.use(validarJWT);
 //Obtener eventos
 router.get('/', getEventos);
 //Crear un nuevo evento
-router.post('/', crearEvento);
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+        check('end', 'Fecha de fin es obligatoria').custom(isDate),
+        validarCampos,
+    ],
+    crearEvento,
+);
 //Actualizar evento
 router.put('/:id', actualizarEvento);
 //Borrar evento
