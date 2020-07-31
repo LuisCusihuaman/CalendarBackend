9af70059fabfbdf13663ea7027b74aaf01ba4625
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 02:53:02 -0300

    chore: configured cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const { dbConnection } = require('./database/config');
+const cors = require('cors');
 require('dotenv').config();
 require('pretty-error').start();
-const { dbConnection } = require('./database/config');
 const app = express();
 
 //Base de datos
 dbConnection();
+
+// CORS
+app.use(cors());
+
 //Public directory
 app.use(express.static('public'));
 
