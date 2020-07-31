e41a1cd1f2bab365b41af2f714f27721c9fcfda3
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 00:21:10 -0300

    style: change 2 spaces for 4

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,39 +1,39 @@
 const { response } = require('express');
 const crearUsuario = (req, res = response) => {
-  const { name, email, password } = req.body;
-  if (name.length < 5) {
-    return res.status(400).json({
-      ok: false,
-      msg: 'El nombre debe de ser de 5 letras',
+    const { name, email, password } = req.body;
+    if (name.length < 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El nombre debe de ser de 5 letras',
+        });
+    }
+    res.json({
+        ok: true,
+        msg: 'registro',
+        name,
+        email,
+        password,
     });
-  }
-  res.json({
-    ok: true,
-    msg: 'registro',
-    name,
-    email,
-    password,
-  });
 };
 
 const loginUsuario = (req, res = response) => {
-  const { email, password } = req.body;
-  res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  });
+    const { email, password } = req.body;
+    res.json({
+        ok: true,
+        msg: 'login',
+        email,
+        password,
+    });
 };
 const revalidarToken = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: 'renew',
-  });
+    res.json({
+        ok: true,
+        msg: 'renew',
+    });
 };
 
 module.exports = {
-  crearUsuario,
-  loginUsuario,
-  revalidarToken,
+    crearUsuario,
+    loginUsuario,
+    revalidarToken,
 };
--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 });
