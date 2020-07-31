d9e8bce1204b6527a504552baa51e7ec7ef1dc3a
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 04:32:08 -0300

    feat: update a event

--- controllers/events.js
+++ controllers/events.js
@@ -16,8 +16,32 @@ const crearEvento = async (req, res = response) => {
         res.status(500).json({ ok: false, msg: 'Hable con el admin...' });
     }
 };
-const actualizarEvento = (req, res = response) => {
-    res.json({ ok: true, msg: 'actualizarEvento' });
+const actualizarEvento = async (req, res = response) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+    try {
+        const evento = await Evento.findById(eventoId);
+        if (!evento) {
+            res.status(404).json({ ok: false, msg: 'Evento no existe con ese id' });
+        }
+        if (evento.user.toString() !== uid) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'No tiene privilegio de editar este evento',
+            });
+        }
+        const nuevoEvento = { ...req.body, user: uid };
+        const eventoActualizado = await Evento.findByIdAndUpdate(eventoId, nuevoEvento, {
+            new: true,
+        });
+        res.json({ ok: true, evento: eventoActualizado });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el admin...',
+        });
+    }
 };
 const eliminarEvento = (req, res = response) => {
     res.json({ ok: true, msg: 'eliminarEvento' });
