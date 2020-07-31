767cc20e0c9ea1bc620b07eb74e644b229b9307c
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 17:01:45 -0300

    fix: send name and uid at renew token request

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,6 +56,8 @@ const revalidarToken = async (req, res = response) => {
     const token = await generarJWT(uid, name);
     res.json({
         ok: true,
+        uid,
+        name,
         token,
     });
 };
