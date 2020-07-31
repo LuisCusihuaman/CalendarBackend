be0ce5aa6b765513b684e34461cf2d22344c9692
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 01:53:47 -0300

    chore(auth): users validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,19 @@
 const { response } = require('express');
 const Usuario = require('../models/Usuario');
 const crearUsuario = async (req, res = response) => {
-    // const { name, email, password } = req.body;
+    const { email, password } = req.body;
     //manejo de errores
     try {
-        const usuario = new Usuario(req.body);
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
+            return res.status(400).json({ ok: false, msg: 'Un usuario existe con ese correo' });
+        }
+        usuario = new Usuario(req.body);
         await usuario.save();
         res.status(201).json({
             ok: true,
-            msg: 'registro',
+            uid: usuario.id,
+            name: usuario.name
         });
     } catch (error) {
         res.status(500).json({ ok: false, msg: 'Porfavor hable con el admin' });
