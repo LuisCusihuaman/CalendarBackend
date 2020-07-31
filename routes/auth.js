c4c1e7888ad67dd951aed11bf5150f4ec2940c07
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-07-31 00:07:47 -0300

    chore: added first auth endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,10 @@
 
 const { Router } = require('express');
 const router = Router();
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
-router.get('/', (req, res) => {
-  res.json({
-    ok: true,
-  });
-});
+router.post('/new', crearUsuario);
+router.post('/', loginUsuario);
+router.get('/renew', revalidarToken);
 
 module.exports = router;
